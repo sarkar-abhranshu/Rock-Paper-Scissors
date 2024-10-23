b82ce0019370c4aaa6d3a23a129539cde35cfe52
sarkar-abhranshu <abhranshusarkar@outlook.com>
2024-10-23 17:38:25 +0530

    Add function which enables user to play 5 rounds of the game

--- rockPaperScissors.js
+++ rockPaperScissors.js
@@ -19,3 +19,39 @@ function getHumanChoice() {
     let hChoice = prompt("Choose between rock, paper and scissors:");
     return hChoice;
 }
+function playRound(humanChoice, computerChoice) {
+    if(humanChoice === computerChoice){
+        console.log(`Draw! Both drew ${humanChoice}.`);
+    }
+    else if((humanChoice === "paper" && computerChoice === "rock") || (humanChoice === "scissors" && computerChoice === "paper") || (humanChoice === "rock" && computerChoice === "scissors")){
+        console.log(`You won! ${humanChoice} beats ${computerChoice}`);
+        humanScore++;
+    }
+    else{
+        console.log(`You lost! ${computerChoice} beats ${humanChoice}`);
+        computerScore++;
+    }
+}
+function playGame() {
+    let round = 1;
+    while(round <= 5){
+        let huChoice = getHumanChoice();
+        let compChoice = getComputerChoice();
+        playRound(huChoice, compChoice);
+        console.log(`Human: ${humanScore}, Computer: ${computerScore}`)
+        round++;
+    }
+    console.log("Game over!");
+    if(humanScore > computerScore) {
+        console.log(`You win!`);
+    }
+    else if(humanScore < computerScore) {
+        console.log(`You lose!`);
+    }
+    else {
+        console.log(`Draw!`)
+    }
+    console.log(`Final Score:\nHuman: ${humanScore}, Computer: ${computerScore}`)
+}
+
+playGame();
